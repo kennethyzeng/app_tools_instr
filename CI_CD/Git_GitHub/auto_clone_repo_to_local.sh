c991c7f3804206a3719326bd9547050515acc947
kennethyzeng <kennethyzeng@gmail.com>
2024-04-23 22:05:17 -0700

    modified and add

---
+++ CI_CD/Git_GitHub/auto_clone_repo_to_local.sh
@@ -0,0 +1,5 @@
+#git clone https://github.com/kennethyzeng/app_tools_instr
+git clone https://github.com/kennethyzeng/stockAnalysisTool
+git clone https://github.com/kennethyzeng/MyLeetcode_By_Topics
+git clone https://github.com/kennethyzeng/webBattles
+git clone https://github.com/kennethyzeng/CPP pythonByTopics_N_SmallProjects
