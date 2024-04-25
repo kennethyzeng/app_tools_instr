ef4e739b364140a020aaabb9072a7dd711cf9f01
kennethyzeng <kennethyzeng@gmail.com>
2024-04-24 20:32:09 -0700

    QA

--- CI_CD/Git_GitHub/auto_clone_repo_to_local.sh
+++ CI_CD/Git_GitHub/auto_clone_repo_to_local.sh
@@ -1,5 +1,6 @@
-#git clone https://github.com/kennethyzeng/app_tools_instr
+git clone https://github.com/kennethyzeng/app_tools_instr
 git clone https://github.com/kennethyzeng/stockAnalysisTool
 git clone https://github.com/kennethyzeng/MyLeetcode_By_Topics
 git clone https://github.com/kennethyzeng/webBattles
-git clone https://github.com/kennethyzeng/CPP pythonByTopics_N_SmallProjects
+git clone https://github.com/kennethyzeng/CPP 
+git clone https://github.com/kennethyzeng/pythonByTopics_N_SmallProjects
