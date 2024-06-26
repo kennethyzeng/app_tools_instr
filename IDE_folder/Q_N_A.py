1b821f92670150257eb8a377630fd7bb22f1ac6b
Kenneth Zeng <51847961+kennethyzeng@users.noreply.github.com>
2024-06-26 11:52:52 -0700

    Create Q_N_A.py

---
+++ IDE_folder/Q_N_A.py
@@ -0,0 +1,16 @@
+#1 when execute C++ code, have below error 
+Starting build...
+/usr/bin/clang -std=gnu++14 -fcolor-diagnostics -fansi-escape-codes -g /Users/SMCI/Documents/CPP/test.cc -o /Users/SMCI/Documents/CPP/test
+Undefined symbols for architecture x86_64:
+
+      ...
+ld: symbol(s) not found for architecture x86_64
+clang: error: linker command failed with exit code 1 (use -v to see invocation)
+
+Build finished with error(s).
+
+ *  The terminal process terminated with exit code: -1. 
+ *  Terminal will be reused by tasks, press any key to close it. 
+
+Solution: 
+
