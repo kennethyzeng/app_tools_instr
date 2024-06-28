dd1ce40fca59dcddbf36e8b8f97b41c9b79dad8a
KennethZeng <kennethyzeng@gmail.com>
2024-06-28 16:19:40 -0700

    add git note

---
+++ CI_CD/Git_GitHub/001_Git_start.py
@@ -0,0 +1,35 @@
+##############1 Git setup ##############
+git config 
+
+#view all of your settings 
+# --show-origin         show origin of config (file, standard input, blob, command line)
+git config --list --show-origin
+
+#set your identity 
+#first time to do after git installation 
+$ git config --global user.name "John Doe"
+$ git config --global user.email johndoe@example.com
+
+
+#check your configuration setting
+git config --list
+
+#config your default text editor 
+#Vim, Emacs and Notepad++ are popular text editors often used by developers on Unix-based systems like Linux and macOS or a Windows system.
+$ git config --global core.editor emacs
+or 
+$ git config --global core.editor "'C:/Program Files/Notepad++/notepad++.exe' -multiInst -notabbar -nosession -noPlugin"
+
+##To set main as the default branch name
+$ git config --global init.defaultBranch main
+
+#a specific key’s value is by typing git config <key>
+git config user.name
+git config user.email 
+git config remote.origin.url
+note: can use "git config --list" to find out all keys 
+
+#For unexpected value and you don't know why 
+#query Git as to the origin for that value, and it will tell you which configuration file had the final say in setting that value
+git config --show-origin rerere.autoUpdate
+
---
+++ CI_CD/Git_GitHub/002 Git_Basic.py
@@ -0,0 +1,8 @@
+"""
+Git basic 
+    -configure and initialize a repository 
+    -begina nd stop tracking files and files pattern
+    -how to undo mistakes quickly and easily 
+    -how to browse the history of your project and view changes between commit 
+    -how to push and pull from remote repositories 
+"""
