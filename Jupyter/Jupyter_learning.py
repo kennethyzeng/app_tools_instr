e89604facc25248aea3e6cf95f5f2a3ddc2ea2ef
Kenneth Zeng <kennethyzeng@gmail.com>
2024-07-08 07:28:48 -0700

    add

---
+++ Jupyter/Jupyter_learning.py
@@ -0,0 +1,10 @@
+#1 Matplotlib inline 
+"""
+is a backend command in Jupyter Notebooks that enables the rendering of Matplotlib plots directly below code cells. 
+This means that when you execute code that generates Matplotlib plots, the plots are displayed inline within the notebook,
+rather than in a separate window or output cell.
+""""
+%Matplotlib inline 
+
+#2 
+%timeit r = [f(x) for x in a]
